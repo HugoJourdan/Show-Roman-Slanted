20bf74bb08f5894da38efb1299d8397d02b026f7
HugoJourd <xhugo.jourdan@gmail.com>
2024-09-04 14:22:54 +0200

    Fix italic bound

--- ShowRomanSlanted.glyphsReporter/Contents/Resources/plugin.py
+++ ShowRomanSlanted.glyphsReporter/Contents/Resources/plugin.py
@@ -14,9 +14,9 @@
 
 from __future__ import division, print_function, unicode_literals
 import objc
-from GlyphsApp import Glyphs, GSAnchor
+from GlyphsApp import Glyphs
 from GlyphsApp.plugins import ReporterPlugin
-from AppKit import NSColor, NSPoint, NSBezierPath
+from AppKit import NSColor, NSPoint, NSAffineTransform, NSColor, NSPoint,NSMinX, NSMaxX, NSMinY, NSMaxY
 import math
 
 
@@ -33,15 +33,14 @@ class ShowRomanSlanted(ReporterPlugin):
 		
 				
 		Font = Glyphs.font
-		scale = Glyphs.font.currentTab.scale
 		selected_master = Font.selectedFontMaster
-
-		
+		scale = Glyphs.font.currentTab.scale
 
 		if selected_master.italicAngle and layer.bezierPath :
 
 			roman_master = self.find_corresponding_roman_italic(layer).master
 			roman_layer = layer.parent.layers[roman_master.id].copyDecomposedLayer()
+			roman_layer.parent = layer.parent
 
 			layer_compareStringNoAnchors = layer.compareString().rsplit('_', 1)[0]
 			roman_layer_compareStringNoAnchors = roman_layer.compareString().rsplit('_', 1)[0]
@@ -49,28 +48,88 @@ class ShowRomanSlanted(ReporterPlugin):
 
 			if layer_compareStringNoAnchors == roman_layer_compareStringNoAnchors :
 
-				roman_layer = layer.parent.layers[0].copyDecomposedLayer()
 
-				layer_compareStringNoAnchors = layer.compareString().rsplit('_', 1)[0]
-				roman_layer_compareStringNoAnchors = roman_layer.compareString().rsplit('_', 1)[0]
-
-				roman_layer = layer.parent.layers[0].copyDecomposedLayer()
-				roman_layer.parent = layer.parent.layers[0].parent
+				roman_layer.associatedMasterId = layer.associatedMasterId
 				self.slant_layer(roman_layer , layer.italicAngle)
 
-
 				layer.font().disableUndo()
-				roman_layer.LSB = layer.bounds.origin.x + (layer.bounds.size.width - roman_layer.bounds.size.width)/2
-				layer.font().enableUndo()
+
+				roman_layer.LSB = layer.LSB
+
+				layer_true_bound = self.getTrueBoundingxBox(layer, layer.bounds)
+				roman_true_bound = self.getTrueBoundingxBox(roman_layer, roman_layer.bounds)
 				
+				roman_width = roman_true_bound[2].x - roman_true_bound[0].x
+				layer_width = layer_true_bound[2].x - layer_true_bound[0].x
+
+				roman_layer.LSB -= roman_true_bound[0].x - layer_true_bound[0].x + (roman_width - layer_width) /2
+
+				layer.font().enableUndo()
+
 				bp = roman_layer.bezierPath
-    
+			
 				NSColor.blueColor().colorWithAlphaComponent_(0.1).set()
 				bp.fill()
-    
+
 				bp.setLineWidth_(1/scale)
 				NSColor.blueColor().colorWithAlphaComponent_(0.3).set()
 				bp.stroke()
+
+
+	@objc.python_method
+	def italicize(self, thisPoint, italicAngle=0.0, pivotalY=0.0):
+		"""
+		Returns the italicized position of an NSPoint 'thisPoint'
+		for a given angle 'italicAngle' and the pivotal height 'pivotalY',
+		around which the italic slanting is executed, usually half x-height.
+		Usage: myPoint = italicize(myPoint,10,xHeight*0.5)
+		"""
+		x = thisPoint.x
+		yOffset = thisPoint.y - pivotalY  # calculate vertical offset
+		italicAngle = math.radians(italicAngle)  # convert to radians
+		tangens = math.tan(italicAngle)  # math.tan needs radians
+		horizontalDeviance = tangens * yOffset  # vertical distance from pivotal point
+		x += horizontalDeviance  # x of point that is yOffset from pivotal point
+		return NSPoint(int(x), int(thisPoint.y))
+
+	@objc.python_method
+	def getTrueBoundingxBox(self, layer, bounds):
+
+		angle = layer.italicAngle
+
+		if angle == 0:
+			# If no italic angle, return the bounding box corners as they are
+			bottom_left = bounds.origin
+			top_left = NSPoint(NSMinX(bounds), NSMaxY(bounds))
+			top_right = NSPoint(NSMaxX(bounds), NSMaxY(bounds))
+			bottom_right = NSPoint(NSMaxX(bounds), NSMinY(bounds))
+			return (bottom_left, top_left, top_right, bottom_right)
+
+		slantHeight = layer.slantHeight()
+
+		# Create a copy of the layer to apply transformations
+		layer.font().disableUndo()
+		layer_copy = layer.copy()
+		layer_copy.parent = layer.parent
+
+		# Apply the skew transformation based on the italic angle
+		transform = NSAffineTransform.new()
+		transform.skew(math.tan(math.radians(-angle)), (0, slantHeight))
+		layer_copy.applyTransform(transform)
+		layer.font().enableUndo()
+
+		# Calculate the new bounding box corners
+		def get_new_point(x, y):
+			pos = NSPoint(x, y)
+			return self.italicize(pos, angle, slantHeight)
+
+		bounds = layer_copy.bounds
+		bottom_left = get_new_point(NSMinX(bounds), NSMinY(bounds))
+		top_left = get_new_point(NSMinX(bounds), NSMaxY(bounds))
+		top_right = get_new_point(NSMaxX(bounds), NSMaxY(bounds))
+		bottom_right = get_new_point(NSMaxX(bounds), NSMinY(bounds))
+
+		return (bottom_left, top_left, top_right, bottom_right)
 
 
 
@@ -103,20 +162,21 @@ class ShowRomanSlanted(ReporterPlugin):
 
 		return None
 
-	#@objc.python_method
-	def italic_skew(self, x, y, angle):
+	@objc.python_method
+	def italicSkew(self, x, y, angle=10.0 ):
 		"""Skews x/y along the x axis and returns skewed x value."""
 		new_angle = ( angle / 180.0 ) * math.pi
 		return x + y * math.tan( new_angle )
 
+
 	#@objc.python_method
-	def slant_layer(self, layer, italicAngle):
+	def slant_layer(self, layer, degree):
 		layer.font().disableUndo()
 
-		skewAngle = italicAngle
+		skewAngle = degree
 		skewRadians = round(math.radians(skewAngle),2)
 		thisMasterXHeight = layer.master.xHeight
-		italicCorrection = self.italic_skew( 0.0, thisMasterXHeight/2.0, skewAngle )
+		italicCorrection = self.italicSkew( 0.0, thisMasterXHeight/2.0, skewAngle )
 		
 		layer.applyTransform([
 								1, # x scale factor
@@ -127,6 +187,7 @@ class ShowRomanSlanted(ReporterPlugin):
 								0  # y position
 								])
 		layer.font().enableUndo()
+
 
 
 
